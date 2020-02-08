a2ad2dfb0befc214752dc50299d181370f1f0e45
ShabazBadshah <badshah.shabaz@gmail.com>
2020-02-08 17:19:13 -0500

    [VES-19] Basic key generation implemented

--- android/app/src/main/java/com/vesta/android/MainActivity.java
+++ android/app/src/main/java/com/vesta/android/MainActivity.java
@@ -3,21 +3,9 @@ package com.vesta.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.telecom.Call;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.Callback;
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,26 +17,12 @@ public class MainActivity extends AppCompatActivity {
         final Button button = (Button)findViewById(R.id.button);
         final TextView responseField = (TextView)findViewById(R.id.textView);
 
-        final OkHttpClient client = new OkHttpClient();
+        PGP pgp = new PGP();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new Thread(new Runnable(){
-                    @Override
-                    public void run() {
-                        try {
-                            final Request request = new Request.Builder()
-                                    .url("http://10.0.2.2:5000/")
-                                    .build();
-                            Response response = client.newCall(request).execute();
-                            Log.v("VESTA-LOG", response.toString());
-                            responseField.setText(response.body().string());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
+//                responseField.setText(response.body().string());
             }
         });
 
