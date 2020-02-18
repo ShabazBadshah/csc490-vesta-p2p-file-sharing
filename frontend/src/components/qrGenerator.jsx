d8c96198809184c696acdbb7d7167e2ed55254fd
hamidtam <tameem.hamid@mail.utoronto.ca>
2020-02-17 20:40:49 -0500

    Create symmetric key on browser

--- frontend/src/components/qrGenerator.jsx
+++ frontend/src/components/qrGenerator.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import QrCode from 'react.qrcode.generator'
+import Enigma from '@cubbit/enigma';
 
 class QrGenerator extends Component {
 
   render() {
 
+    const key = Enigma.AES.create_key()
+    // textEnd = TextEncoder("utf-8").encode(myString);
+    const textDec = new TextDecoder("utf-8").decode(key).substring(0, 12);
+
+    console.log(textDec)
+
+    // const sharedSecret = crypto.randomBytes(16); // 128-bits === 16-bytes
+    // const textSecret = sharedSecret.toString('base64');
+
     // random 24 character string
-    const qrValue = Math.random().toString(36).slice(-12).concat(
-    Math.random().toString(36).slice(-12))
+    // const qrValue = Math.random().toString(36).slice(-12).concat(
+    // Math.random().toString(36).slice(-12))
 
     return (
 
@@ -16,7 +26,7 @@ class QrGenerator extends Component {
       <center> <h1 style={{fontSize: '72px'}}> Vesta.io </h1> </center>
 
       <div>
-      <center> <QrCode value={qrValue}/> </center>
+      <center> <QrCode value={textDec}/> </center>
       </div>
 
       </div>
