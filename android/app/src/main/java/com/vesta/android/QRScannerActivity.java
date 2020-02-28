235d7106f9245ca40a544e7e3a16f7e0f56756e6
kunalSHU <kunalshukla@hotmail.com>
2020-02-28 11:10:59 -0500

    Added storage of multiple keys based on unique device id

--- android/app/src/main/java/com/vesta/android/QRScannerActivity.java
+++ android/app/src/main/java/com/vesta/android/QRScannerActivity.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import java.security.Key;
+import android.provider.Settings.Secure;
+
 
 import com.google.zxing.Result;
 import com.google.zxing.MultiFormatReader;
@@ -73,11 +75,16 @@ public class QRScannerActivity extends AppCompatActivity implements ZXingScanner
     @Override
     public void handleResult(Result rawResult) {
 
-        //Storing the rawResult which is the public key in shared preferences
+        //Storing the rawResult which is the public key in shared preferences with the
+        // unique identifier of the device
+        String androidID = Secure.getString(this.getBaseContext().getContentResolver(),
+                Secure.ANDROID_ID);
+
         KeyPairManager.storePublicKeySharedPref(SHARED_PREFERENCES, this.getBaseContext(),
-                rawResult.getText());
+                rawResult.getText(), androidID);
         Log.i("Retrieve shared pref",
-                KeyPairManager.retrievePublicKeySharedPref(SHARED_PREFERENCES, this.getBaseContext()));
+                KeyPairManager.retrievePublicKeySharedPref(SHARED_PREFERENCES, this.getBaseContext(),
+                        androidID));
 
         System.out.println(((TextView)findViewById(R.id.textView)));
         Log.v(TAG, rawResult.getText()); // Prints scan result
