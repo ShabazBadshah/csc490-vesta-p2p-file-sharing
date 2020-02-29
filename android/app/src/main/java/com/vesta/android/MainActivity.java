f24ff03ee4972df967ae77be2dcbf2033ee0583d
ShabazBadshah <badshah.shabaz@gmail.com>
2020-02-29 15:17:08 -0500

    [VES-3] Implemented splash screen

--- android/app/src/main/java/com/vesta/android/MainActivity.java
+++ android/app/src/main/java/com/vesta/android/MainActivity.java
@@ -97,8 +97,8 @@ public class MainActivity extends AppCompatActivity {
         scanQRButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), QRScannerActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(view.getContext(), QRScannerActivity.class);
+//                startActivity(intent);
             }
         });
 
--- android/app/src/main/java/com/vesta/android/SplashScreenActivity.java
+++ android/app/src/main/java/com/vesta/android/SplashScreenActivity.java
@@ -16,7 +16,7 @@ public class SplashScreenActivity extends Activity{
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashScreenActivity.this, ActivityQRMain.class);
+                Intent intent=new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
