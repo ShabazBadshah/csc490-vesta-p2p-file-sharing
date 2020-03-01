da1b123f855a62dc0393c76272b9027ede073b1d
ShabazBadshah <badshah.shabaz@gmail.com>
2020-02-29 22:37:45 -0500

    Aligned master with feature branch

--- android/app/src/main/java/com/vesta/android/SplashScreenActivity.java
+++ android/app/src/main/java/com/vesta/android/SplashScreenActivity.java
@@ -4,23 +4,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+
 public class SplashScreenActivity extends Activity{
 
-    Handler handler;
+    public static final int SPLASH_SCREEN_DELAY_MILLIS = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
+        try {
+            KeyPairManager keyPairManager = new KeyPairManager(getApplicationContext());
+            keyPairManager.generateRsaEncryptionKeyPair("userKeys");
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableEntryException e) {
+            e.printStackTrace();
+        }
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent=new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        }, SPLASH_SCREEN_DELAY_MILLIS);
 
     }
 }
