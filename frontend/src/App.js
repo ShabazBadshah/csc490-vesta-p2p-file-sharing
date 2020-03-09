617d8d8e3310edb9f24cc7e1e5a9f7c8da1ebe46
hamidtam <tameem.hamid@mail.utoronto.ca>
2020-03-08 20:02:52 -0400

    route system added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import background from './background.jpg';
 import {
@@ -22,24 +22,29 @@ import {
 import QrGenerator from './components/qrGenerator';
 import Navigation from './components/navigation';
 import Menu from './components/menu';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
+import MainPage from './pages'
 
-function App() {
+
+class App extends Component {
+  render() {
 
   return (
 
-    <div>
+    <Router>
+      <Route path="/" component={MainPage} />
+    </Router>
 
-      <p style={{color: "#905EAF", position: "absolute", left: "90px", top: "10px", fontSize: "72px"}}> Vesta </p>
-      <p style={{color: "#black", position: "absolute", left: "100px", top: "105px", fontSize: "18px"}}> Secure file sharing in your control  </p>
+    );
 
-    <Navigation> </Navigation>
+  }
 
-    <Menu> </Menu>
-
-    </div>
-
-
-  );
 }
 
 export default App;
