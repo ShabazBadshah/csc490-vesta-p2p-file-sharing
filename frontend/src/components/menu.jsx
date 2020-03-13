34eb81efc7edfb688d5daa418485d0bdaa901296
Muhammad Waleed <muhammad.waleed01@gmail.com>
2020-03-13 10:00:29 -0400

    front-end webrtc code

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -30,7 +30,16 @@ class Menu extends Component {
     this.privacyAct = this.privacyAct.bind(this);
     this.helpAct = this.helpAct.bind(this);
     this.contactAct = this.contactAct.bind(this);
+    this.connectPeers = this.connectPeers.bind(this);
+    this.receiveChannelCallback = this.receiveChannelCallback.bind(this)
+    this.handleSendChannelStatusChange = this.handleSendChannelStatusChange.bind(this);
     this.state = {home: true, privacy: false, help: false ,contact: false}
+
+    this.localConnection = null;   // RTCPeerConnection for our "local" connection
+    this.remoteConnection = null;  // RTCPeerConnection for the "remote"
+    
+    this.sendChannel = null;       // RTCDataChannel for the local (sender)
+    this.receiveChannel = null;    // RTCDataChannel for the remote (receiver)
   }
 
   homeAct() {
@@ -48,6 +57,59 @@ class Menu extends Component {
   contactAct() {
     this.setState({home: false, privacy: false, help: false ,contact: true});
   }
+  connectPeers() {
+    this.localConnection = new RTCPeerConnection();
+
+    this.sendChannel = this.localConnection.createDataChannel("sendChannel");
+    this.sendChannel.onopen = this.handleSendChannelStatusChange;
+    this.sendChannel.onclose = this.handleSendChannelStatusChange;
+    
+    this.remoteConnection = new RTCPeerConnection();
+    this.remoteConnection.ondatachannel = this.receiveChannelCallback;
+
+    this.localConnection.onicecandidate = e => !e.candidate
+    || this.remoteConnection.addIceCandidate(e.candidate)
+    .catch((error) => console.log("addICECandidate failed: " + error.toString()));
+
+    this.remoteConnection.onicecandidate = e => !e.candidate
+    || this.localConnection.addIceCandidate(e.candidate)
+    .catch((error) => console.log("addICECandidate failed!: " + error.toString()));
+    
+    this.localConnection.createOffer()
+    .then(offer => this.localConnection.setLocalDescription(offer))
+    .then(() => this.remoteConnection.setRemoteDescription(this.localConnection.localDescription))
+    .then(() => this.remoteConnection.createAnswer())
+    .then(answer => this.remoteConnection.setLocalDescription(answer))
+    .then(() => this.localConnection.setRemoteDescription(this.remoteConnection.localDescription))
+    .catch((error) => console.log("Unable to create an offer: " + error.toString()));
+
+    console.log("sup");
+  }
+  handleSendChannelStatusChange() { // Fix
+    if (this.sendChannel) {
+      var state = this.sendChannel.readyState;
+    
+      if (state === "open") {
+        messageInputBox.disabled = false;
+        messageInputBox.focus();
+        sendButton.disabled = false;
+        disconnectButton.disabled = false;
+        connectButton.disabled = true;
+      } else {
+        messageInputBox.disabled = true;
+        sendButton.disabled = true;
+        connectButton.disabled = false;
+        disconnectButton.disabled = true;
+      }
+    }
+  }
+
+  receiveChannelCallback() {
+    this.receiveChannel = event.channel;
+    this.receiveChannel.onmessage = handleReceiveMessage;//TODO
+    this.receiveChannel.onopen = handleReceiveChannelStatusChange;//TODO
+    this.receiveChannel.onclose = handleReceiveChannelStatusChange;//TODO
+  }
 
   render(){
 
@@ -59,8 +121,10 @@ class Menu extends Component {
     let cardBody;
 
     if(homeTab){
-      cardBody = <span>
-      </span>
+      cardBody = <div> <FormInput placeholder="Enter Link" className="message" style={{width: "360px", borderColor: "#905EAF"}}/>
+      <Button theme="light" style={{color: 'white', borderColor: "#905EAF", backgroundColor: "#905EAF"}} > Send message </Button>
+      <Button theme="light" style={{color: 'white', borderColor: "#905EAF", backgroundColor: "#905EAF"}} onClick={this.connectPeers}> Connect </Button>
+      </div>
       cardTitle = <h5>  </h5>
     }
 
