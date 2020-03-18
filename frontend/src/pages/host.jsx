e5ad470df630c853dfed796d33a98c91a5b15b0d
kunalSHU <kunalshukla@hotmail.com>
2020-03-18 16:13:03 -0400

    Host jsx file

--- frontend/src/pages/host.jsx
+++ frontend/src/pages/host.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import HostNav from '../components/hostNav';
 import Menu from '../components/menu';
 
+
+const submitClick = () => {
+  var P2P = require('socket.io-p2p');
+  var io = require('socket.io-client');
+  var socket = io("http://3b3e9cb4.ngrok.io");
+  var p2pSocket = new P2P(socket);
+
+  var text = document.getElementById("submitText").value;
+
+  socket.emit('peer-msg', {textVal : text});
+  console.log("IT CLICKED")
+
+}
+
 const HostPage = () => {
+
 
   return (
     <div>
@@ -10,7 +25,14 @@ const HostPage = () => {
 
     <Menu> </Menu>
 
+    <input id="submitText" type="text"/>
+    <button type="submit" onClick={submitClick}>Submit</button>
+
+
     </div>
+
+    
+
   );
 };
 
