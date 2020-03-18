84f253bd4d09210d468d4f89f07efb0cb1c45bf0
Shabaz Badshah <badshah.shabaz@gmail.com>
2020-03-18 13:04:51 -0400

    [VES-45] Restoring deleted frontend files from PR#25 (#28)
    Co-authored-by: Swetha Maramganty <swethamaramganty@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import background from './background.jpg';
+import React, { Component } from "react";
+import "./App.css";
+import background from "./background.jpg";
 import {
   Card,
   CardHeader,
@@ -14,25 +14,40 @@ import {
   ButtonToolbar,
   FormInput,
   InputGroup,
+  Nav,
+  NavItem,
+  NavLink,
   Collapse
 } from "shards-react";
-import QrGenerator from './components/qrGenerator';
-import Navigation from './components/navigation';
+import QrGenerator from "./components/qrGenerator";
+import Navigation from "./components/navigation";
+import Menu from "./components/menu";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/Host" component={HostPage} />
-        <Route path="/Receive" component={ReceivePage} />
-        <Route exact path="/404" component={ErrorPage} />
-      <Redirect to="/404"/>
-      </Switch>
-    </Router>
+import MainPage from "./pages/main";
+import ErrorPage from "./pages/404error";
+import HostPage from "./pages/host";
+import ReceivePage from "./pages/receive";
 
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/Host" component={HostPage} />
+          <Route path="/Receive" component={ReceivePage} />
+          <Route exact path="/404" component={ErrorPage} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
     );
-
   }
 }
 
