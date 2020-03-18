80d2c12fa803367cad4713ac5700d5d39c6aad3a
kunalSHU <kunalshukla@hotmail.com>
2020-03-18 16:13:03 -0400

    Host jsx file

--- frontend/src/pages/host.jsx
+++ frontend/src/pages/host.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import QrGenerator from '../components/qrGenerator';
 
+
+const submitClick = () => {
+  var P2P = require('socket.io-p2p');
+  var io = require('socket.io-client');
+  var socket = io("http://3b3e9cb4.ngrok.io");
+  var p2pSocket = new P2P(socket);
+
+  var text = document.getElementById("submitText").value;
+
+  socket.emit('peer-msg', {textVal : text});
+  console.log("IT CLICKED")
+
+}
+
 const HostPage = () => {
+
 
   return (
     <div>
@@ -10,7 +25,14 @@ const HostPage = () => {
 
     <QrGenerator> </QrGenerator>
 
+    <input id="submitText" type="text"/>
+    <button type="submit" onClick={submitClick}>Submit</button>
+
+
     </div>
+
+    
+
   );
 };
 
