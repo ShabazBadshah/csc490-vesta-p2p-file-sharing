ecd8fd97798bf79fe06f39e926af23005f22b249
kunalSHU <kunalshukla@hotmail.com>
2020-03-19 18:37:32 -0400

    Recieving msg from server

--- frontend/src/pages/host.jsx
+++ frontend/src/pages/host.jsx
@@ -6,13 +6,19 @@ import Menu from '../components/menu';
 const submitClick = () => {
   var P2P = require('socket.io-p2p');
   var io = require('socket.io-client');
-  var socket = io("http://3b3e9cb4.ngrok.io");
+  var socket = io("http://13be2d76.ngrok.io");
   var p2pSocket = new P2P(socket);
 
   var text = document.getElementById("submitText").value;
 
+  //send msg to server on peer-msg channel
   socket.emit('peer-msg', {textVal : text});
   console.log("IT CLICKED")
+
+  //recieve msg from server from the channel
+  socket.on('peer-msg', function(msg){
+    console.log(msg);
+  });
 
 }
 
