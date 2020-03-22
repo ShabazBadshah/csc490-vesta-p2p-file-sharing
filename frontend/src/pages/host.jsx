948cc8bea89e02cd5a70adf1c865716a8a9feceb
kunalSHU <kunalshukla@hotmail.com>
2020-03-22 11:47:34 -0400

    Refactoring code

--- frontend/src/pages/host.jsx
+++ frontend/src/pages/host.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import QrGenerator from '../components/qrGenerator';
 
-
+//const submit click function JUST for TESTING purposes
+//will be REMOVED when needed 
 const submitClick = () => {
   var P2P = require('socket.io-p2p');
   var io = require('socket.io-client');
-  var socket = io("http://13be2d76.ngrok.io");
+  var socket = io("http://21bda266.ngrok.io");
   var p2pSocket = new P2P(socket);
 
   var text = document.getElementById("submitText").value;
 
-  //send msg to server on peer-msg channel
+    //recieve msg from server from the channel
+  //will get the encrypted sym key
   socket.emit('peer-msg', {textVal : text});
-  console.log("IT CLICKED")
 
-  //recieve msg from server from the channel
   socket.on('peer-msg', function(msg){
+    console.log("IN HERERE")
     console.log(msg);
   });
+  //send msg to server on peer-msg channel
+  console.log("IT CLICKED")
+
 
 }
 
