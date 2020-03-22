b9b15ce81732fff43d5967aed39086826c37adb3
daveavi <avidave97@gmail.com>
2020-03-22 17:41:20 -0400

    just updated file streaming on server side

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var server = express();
-var ecstatic = require('ecstatic')
+var ecstatic = require('ecstatic');
+var ss = require('socket.io-stream');
+var path = require('path');
+var fs = require('fs');
 server = require('http').createServer(
   ecstatic({ root: __dirname, handleError: false })
 )
@@ -27,13 +30,32 @@ io.on('connection', function (socket) {
   console.log("PRINT SOMETHING PLZ")
   console.log("one user connected " + socket.id)
   socket.on('peer-msg', function (data) {
-    console.log('Message jbjkbfrom peer: %s', data.textVal)
-
+    console.log('Message jbjkbfrom peer: %s', data)
+    // ss(socket).on('file', function(stream) {
+    //   fs.createReadStream('./testFile').pipe(stream);
+    // });
     //broadcast will send msg to everyone exept the sender
     //use only emit to send to everyone
-    socket.broadcast.emit('peer-msg', data)
+    // socket.broadcast.emit('peer-msg', data);
+    // ss(socket).on('test-stream', function(stream, data2) {
+    //   console.log("File is being written");
+    //   console.log("About to write: %s", data2.name);
+    //   var filename = path.basename(data2.name);
+    //   stream.pipe(fs.createWriteStream(filename));
+    // });
     
+    // ss(socket).emit('test-', stream, {name: filename});
+    // fs.createReadStream(filename).pipe(stream);
   })
+
+
+  ss(socket).on('test-stream', function(stream, data2) {
+    console.log("File is being written");
+    console.log("About to write: %s", data2.name);
+    var filename = path.basename(data2.name);
+    stream.pipe(fs.createWriteStream(filename));
+  });
+
   socket.on('go-private', function(data) {
     socket.broadcast.emit('go-private', data);
   });
