b7f22a704023f7cc6f68e09c2fa47b5200a13844
Muhammad Waleed <muhammad.waleed01@gmail.com>
2020-03-26 14:25:43 -0400

    addeed encryption  and decryption

--- frontend/src/pages/host.jsx
+++ frontend/src/pages/host.jsx
@@ -9,17 +9,21 @@ var sendChannel = null;       // RTCDataChannel for the local (sender)
 var receiveChannel = null;    // RTCDataChannel for the remote (receiver)
 
 var P2P = require('socket.io-p2p');
+var key = "key"
+var crypto = require('crypto')
 var io = require('socket.io-client');
-var socket = io("http://c02ffb6d.ngrok.io");
+var socket = io("http://5072cabb.ngrok.io");
 var opts = {autoUpgrade: false, numClients: 10};
 var p2pSocket = new P2P(socket, opts)
 p2pSocket.on('peer-msg', function (data) {
-  console.log('From a peer %s', data.textVal);
+  var dec = crypto.createDecipher("aes-256-ctr",key).update(data.textVal,"hex","utf8");
+  console.log('From a peer encrypted: %s', data.textVal);
+  console.log('From a peer decrypted: %s', dec);
 });
 p2pSocket.on('go-private', function () {
   p2pSocket.upgrade(); // upgrade to peerConnection
   privateClick();
-});
+});  
 const privateClick = () => {
   socket.emit('go-private', true);
   p2pSocket.useSockets = false;
@@ -28,8 +32,8 @@ const privateClick = () => {
 
 const submitClick = () => {
   var text = document.getElementById("submitText").value;
-
-  p2pSocket.emit('peer-msg', {textVal : text});
+  var enc = crypto.createCipher("aes-256-ctr",key).update(text,"utf-8","hex");
+  p2pSocket.emit('peer-msg', {textVal : enc});
   console.log("IT CLICKED")
 }
 
